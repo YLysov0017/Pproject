eee7479b6527bedf9381ab938fd28ac6c56e9867
YLysov0017 <amdradeon97@mail.ru>
2021-10-09 13:58:10 +1000

    bvggfgftgfft

--- main.py
+++ main.py
@@ -1,26 +1,19 @@
-import time
-from tkinter import Tk, Label, mainloop 
-
-root = Tk()
-root.title('clocks')
-root.geometry('+10+10')
-root.overrideredirect(True)
-
-lb = Label(root, bg='black', fg='purple', text='dfh', font = 'arial 210')
-lb.pack(padx=10, pady=20)
-
-
-
-def gettime():
-    p = ':' if int(time.strftime('%S')) % 2 == 0 else ' '
-    lb['text'] = time.strftime(f'%H{p}%M{p}%S')
-    lb.after(1000, gettime)
-
-def close(event):
-    root.destroy()
-
-    
-root.bind("<Escape>", close)
-lb.after_idle(gettime)
-root.mainloop()
-.0
+#a = [4, 8, 5]
+#b = [7, 8, 7]
+#res = []
+#for c in a:
+#        res.append(c*d)
+#print(res)
+import random
+a = []
+for v in range(11):
+    k = random.randint(0, 500)
+    a.append(k)
+summa = 0
+ss = 0
+for i in a:
+    summa += i
+    ss += 1
+avg = summa / ss
+print(a)  
+print(ss, avg, summa)
---
+++ xyerga.py
@@ -0,0 +1,5 @@
+import random
+listik = [[random.randint(10, 20) for i in range(2)] for j in range(2)]
+print(listik)
+a = listik.
+print(a)
