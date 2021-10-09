3100d9cf0236bf668e35ca2cf05cfe6c7ae96bce
YLysov0017 <amdradeon97@mail.ru>
2021-10-09 14:34:15 +1000

    залил дызэ

--- DZ.py
+++ DZ.py
@@ -6,6 +6,7 @@
 '''
 программа прверяет, является ли строка палиндромом
 '''
+'''
 import random
 listik = [random.randint(0, 1000) for i in range(100)]
 print('Вывод нечетных чисел')
@@ -15,3 +16,17 @@ for i in listik:
         if i == 195:
             print(i, 'найдено ключевое число')
             break
+'''
+
+stroka = input()
+def is_palindrom(stroka):
+    for i in range(1, len(stroka)):
+        if stroka[i - 1] == stroka[-i]:
+            continue
+        else:
+            print('не является палиндромом')
+            break
+    else:
+        print('является палиндромом')
+    return 'the end'
+print(is_palindrom(stroka))
