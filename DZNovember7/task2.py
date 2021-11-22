78464f4a99883100ca1f113fafc29da192d9f326
YLysov0017 <91357032+YLysov0017@users.noreply.github.com>
2021-11-22 12:00:58 +1000

    Delete DZNovember7 directory

--- DZNovember7/task2.py
+++
@@ -1,8 +0,0 @@
-def spisokchisel(n):
-    a = []
-    a.append(n - 1)
-    a.append(n + 1)
-    return(a)
-
-n = int(input())
-print(spisokchisel(n))
--- DZNovember7/task3.py
+++
@@ -1,6 +0,0 @@
-def spisokslov(string):
-    string = string.lower()
-    string = string.rsplit()
-    return string
-string = input()
-print(spisokslov(string))
